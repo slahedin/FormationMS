b379c8755ae818a27ca79aba33204ef2911a43c1
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-16 14:11:45 +0000

    config server remote and local properties

---
+++ slahedin/src/test/java/tn/gov/sibtel/slahedin/SlahedinApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.slahedin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlahedinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
