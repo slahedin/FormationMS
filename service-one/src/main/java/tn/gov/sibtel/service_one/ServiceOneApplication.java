43cc1287d60c78a235cea988c50436335d186337
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-16 16:03:42 +0000

    run service one

--- service-one/src/main/java/tn/gov/sibtel/service_one/ServiceOneApplication.java
+++ service-one/src/main/java/tn/gov/sibtel/service_one/ServiceOneApplication.java
@@ -2,12 +2,24 @@ package tn.gov.sibtel.service_one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ServiceOneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceOneApplication.class, args);
 	}
 
+}
+@RestController
+class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Service One!";
+    }
 }
