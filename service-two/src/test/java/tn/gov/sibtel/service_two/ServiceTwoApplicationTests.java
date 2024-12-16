2975bfc262f8ce664d799721340bd8c33fc4066a
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-16 15:59:49 +0000

    test communication between services

---
+++ service-two/src/test/java/tn/gov/sibtel/service_two/ServiceTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.service_two;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceTwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
