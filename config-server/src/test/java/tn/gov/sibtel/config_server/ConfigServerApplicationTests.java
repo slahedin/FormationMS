cdf5eb1bd19dd6c95d82864d674622f4f4fb9fc1
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-16 13:23:55 +0000

    test config server

---
+++ config-server/src/test/java/tn/gov/sibtel/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
