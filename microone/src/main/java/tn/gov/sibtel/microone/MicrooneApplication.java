a02f5e823bf2ad128541488ae01f510a54700394
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-18 09:48:25 +0000

    microone

---
+++ microone/src/main/java/tn/gov/sibtel/microone/MicrooneApplication.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.microone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrooneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrooneApplication.class, args);
+	}
+
+}
