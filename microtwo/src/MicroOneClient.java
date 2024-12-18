85b4524813e414ac547358d33afa1fdc99559337
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-18 09:42:04 +0000

    test eureka

--- microtwo/src/MicroOneClient.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@FeignClient(name = "microone")
-public interface MicroOneClient {
-
-    @GetMapping("/hello")
-    String getHelloMessage();
-}
