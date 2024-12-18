fd6884b3990a34eab72af87a5209fd5825807a80
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-18 08:47:42 +0000

    microtwo

---
+++ microtwo/src/main/java/tn/gov/sibtel/microtwo/MicrotwoApplication.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.microtwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrotwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrotwoApplication.class, args);
+	}
+
+}
---
+++ microtwo/src/test/java/tn/gov/sibtel/microtwo/MicrotwoApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.microtwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrotwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
