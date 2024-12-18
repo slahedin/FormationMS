31e8414df54e06d5d81f46fe9daf6a30cf780dad
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-18 08:10:52 +0000

    microone service

---
+++ microone/src/test/java/tn/gov/sibtel/microone/MicrooneApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.microone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrooneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
