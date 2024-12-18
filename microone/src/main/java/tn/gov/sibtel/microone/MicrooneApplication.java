382d81675a33a7d6ed7a8aac2bd49c4526c1bb31
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-18 08:16:56 +0000

    microone

--- microone/src/main/java/tn/gov/sibtel/microone/MicrooneApplication.java
+++ microone/src/main/java/tn/gov/sibtel/microone/MicrooneApplication.java
@@ -2,12 +2,24 @@ package tn.gov.sibtel.microone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicrooneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicrooneApplication.class, args);
 	}
 
+}
+
+@RestController
+class MessageController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from microone";
+    }
 }
