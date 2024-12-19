f2714c3f0006a84582d0ff2270025c6531afab0c
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-19 08:05:57 +0000

    micro1

---
+++ micro1/src/main/java/tn/gov/sibtel/micro1/Micro1Application.java
@@ -0,0 +1,15 @@
+package tn.gov.sibtel.micro1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Micro1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Micro1Application.class, args);
+	}
+
+}
---
+++ micro1/src/test/java/tn/gov/sibtel/micro1/Micro1ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.gov.sibtel.micro1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Micro1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
