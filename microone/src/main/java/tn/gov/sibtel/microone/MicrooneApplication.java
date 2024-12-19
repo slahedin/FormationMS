184ba44bf2ebe32f92d2fa8a87c04da8658348ce
chaabeni slah edin <47463609+slahedin@users.noreply.github.com>
2024-12-19 10:17:42 +0000

    test

--- microone/src/main/java/tn/gov/sibtel/microone/MicrooneApplication.java
+++ microone/src/main/java/tn/gov/sibtel/microone/MicrooneApplication.java
@@ -2,12 +2,25 @@ package tn.gov.sibtel.microone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicrooneApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicrooneApplication.class, args);
 	}
-
+	@RestController
+	public class HelloController {
+	
+		@GetMapping("/hello")
+		public String hello() {
+			return "Hello from microservice 1!";
+		}
+	}
 }
